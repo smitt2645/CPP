81a517a7f4b993b157bb7a2fbdc675f88774e39f
Smit Trivedi <smit.iihglobal@gmail.com>
2025-07-01 10:53:36 +0530

    first commit

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// if any starting from # called as pre processor directives
+
+//  way to use namespace
+// 1) using namespace std;
+// 2) using std::cout;
+// 3) std::cout -> direct inside method;
+
+// here we have a main() called as method and function also !
+int main(){
+    std::cout << "Smit Trivedi" << std::endl;
+}
