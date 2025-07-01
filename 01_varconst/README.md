9869d29d54f6b0f6f240c80430e29d62d4e0bea0
Smit Trivedi <smit.iihglobal@gmail.com>
2025-07-01 15:10:02 +0530

    basic of dtypes

---
+++ 01_varconst/README.md
@@ -0,0 +1,4 @@
+# Document Heading
+
+This is a document with a top-level heading
+<!-- here we are going to start a new chapter with cPP -->
---
+++ 01_varconst/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+    const int aadhar_number = 100987656;
+    
+    cout << "line 1" << endl;
+    cout << "line 2";
+    cout << " count";
+    return 0; 
+}
