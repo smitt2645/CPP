f5d576012f9205daff9c762933d826a590eaa6dd
Smit Trivedi <smit.iihglobal@gmail.com>
2025-07-03 09:58:16 +0530

    a sample program done !

--- 01_varconst/main.cpp
+++ 01_varconst/main.cpp
@@ -5,9 +5,12 @@ using std::endl;
 
 int main(){
     const int aadhar_number = 100987656;
-    
+    bool isIndian = true;
+
     cout << "line 1" << endl;
-    cout << "line 2";
-    cout << " count";
+    cout << "line 2" << endl;
+    cout << " count" << endl;
+    cout << "is indian " <<  isIndian << endl;
+    // cout <<   << endl;
     return 0; 
 }
--- test.cpp
+++ test.cpp
@@ -6,7 +6,25 @@
 // 2) using std::cout;
 // 3) std::cout -> direct inside method;
 
+using std::cout;
+using std::endl;
+using std::cin;
 // here we have a main() called as method and function also !
 int main(){
     std::cout << "Smit Trivedi" << std::endl;
+    int val_1 = 1;
+    int val_2 = 1;
+    int user_input;
+
+    cout << "val_1 * val_2 : " << val_1 * val_2 << endl ;
+    cout << "Enter a number : " ;
+    cin >> user_input;
+
+    if(user_input > 10){
+        cout << "great !";
+    }else{
+        cout << "please provide valid user input !";
+    }
+
+    return 0;
 }
